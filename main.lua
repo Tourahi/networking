a4932f6596bc5d8c26f9c6e5c787865a6f6c0a05
maromaroXD <amine-fb-fb@hotmail.fr>
2021-03-16 21:56:31 +0100

    Refactoring :: make the net table unique.

---
+++ main.lua
@@ -0,0 +1,22 @@
+local net_service = require('services.net');
+
+
+function love.load(arg)
+
+end
+
+function love.draw()
+
+end
+
+function love.draw()
+
+end
+
+function love.update(dt)
+  net_service:update();
+end
+
+function love.quit()
+
+end
--- services/net.lua
+++ services/net.lua
@@ -1,41 +1,53 @@
 local enet = require('enet');
-local net = {};
+local net = {
+  -- config Properties
+  port = '6789',
+  ip = 'localhost',
+  -- server Properties
+  peers = {},
 
-local start_server = function(self)
-  self.server_host = enet.host_create(self.ip..':'..self.port);
+  received_data = false
+};
+
+-- Populate one or the other depending if you start a server or client.
+local client_host;
+local server_host;
+
+net.start_server = function(self)
+  server_host = enet.host_create(self.ip..':'..self.port);
 end
 
-local start_client = function(self)
-  self.client_host = enet.host_create();
-  self.server_host = self.client_host:connect(self.ip..':'..self.port);
+net.start_client = function(self)
+  client_host = enet.host_create();
+  server_host = client_host:connect(self.ip..':'..self.port);
 end
 
-local is_connected = function(self)
+net.is_connected = function(self)
   return self.received_data;
 end
 
-local is_client = function(self)
-  return self.client_host and true or false;
+net.is_client = function(self)
+  return client_host and true or false;
 end
 
-local is_server = function(self)
-  return self.server_host and not self.client_host;
+net.is_server = function(self)
+  return server_host and not client_host;
 end
 
-local disconnect = function(self)
-  for _, self.peer ipairs(self.peers) do
+net.disconnect = function(self)
+  for _, peer in ipairs(self.peers) do
     self.peer:disconnect_now();
   end
   self.peers = {};
   if self.is_client() then
     self.server_host:disconnect_now();
-    self.client_host = nil;
+    client_host = nil;
   end
   self.server_host = nil;
   self.received_data = false;
 end
 
-local send = function(self, message)
+net.send = function(self, message)
   if self.is_client() then
     server_host:send(message); -- here server_host is the peer
   else
@@ -43,8 +55,8 @@ local send = function(self, message)
   end
 end
 
-local update = function(self)
-  local host = self.client_host or self.server_host;
+net.update = function(self)
+  local host = client_host or server_host;
   if not host then return end
   local event = host:service();
   if event then
@@ -55,33 +67,6 @@ local update = function(self)
       print(k, v);
     end
   end
-end
-
-
-net.create = function(port, ip)
-  local obj = {};
-  -- config Properties
-  obj.port = port or '6789';
-  obj.ip = ip or 'localhost';
-  -- Populate one or the other depending if you start a server or client.
-  obj.client_host = nil;
-  obj.server_host = nil;
-  -- server Properties
-  local peers = {};
-
-  local received_data = false;
-
-  -- Methods
-  self.start_server = start_server;
-  self.start_client = start_client;
-  self.is_connected = is_connected;
-  self.is_client = is_client;
-  self.is_server = is_server;
-  self.disconnect = disconnect;
-  self.send = send;
-  self.update = update;
-
-  return obj;
 end
 
 return net;
